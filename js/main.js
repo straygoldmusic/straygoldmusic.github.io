8be92a83f4083385d89f7cca05d511168962fed0
Jeana Smith <jelsmith@cspire.com>
2024-01-31 21:58:10 -0600

    - Change body font - Fix release date - Remove 1px border that was still showing on the slide out menu - Remove small blue dot that was showing next to the Apple Music logo - Attempt to fix the issue where sometimes tapping on the menu toggler doesn't update the icon

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
-let div = document.getElementById("menuIcon")
+let div = document.getElementById("clickWrapper")
 
 div.addEventListener('click', () => {
-    if (div.classList.contains("close")) {
-        div.className = "menu animate"
+    console.log('toggling');
+    console.log(div.children[0].classList);
+    if (div.children[0].classList.contains("close")) {
+        div.children[0].className = "menu animate"
     } else {
-        div.className = "close animate"
+        div.children[0].className = "close animate"
     }
 })
