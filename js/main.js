6f9f770c5a1a162d3688be30fc03bfb111fabf06
Jeana Smith <jelsmith@cspire.com>
2024-02-01 09:05:54 -0600

    Add footer with links, credits, and copyright

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-let div = document.getElementById("clickWrapper")
+let navToggle = document.getElementById("navbar-toggler")
 
-div.addEventListener('click', () => {
-    console.log('toggling');
-    console.log(div.children[0].classList);
-    if (div.children[0].classList.contains("close")) {
-        div.children[0].className = "menu animate"
+navToggle.addEventListener('click', () => {
+    if (navToggle.children[0].children[0].classList.contains("close")) {
+        navToggle.children[0].children[0].className = "menu animate"
     } else {
-        div.children[0].className = "close animate"
+        navToggle.children[0].children[0].className = "close animate"
     }
 })
